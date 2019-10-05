62762e3ce678909b3a01b7436c757c7d57421dcf
Furkan Doğu <37083315+furkanDogu@users.noreply.github.com>
2019-10-05 18:13:14 +0300

    Clean test query users

--- src/graphql/queries/user.ts
+++ src/graphql/queries/user.ts
@@ -1,21 +1,5 @@
-const levels = [{ game_id: "1", level: 5 }];
-const usersArr = [
-  {
-    id: "123",
-    email: "furkandogu35@gmail.com",
-    name: "furkan",
-    surname: "doğu",
-    password: "123123",
-    profileName: "furki",
-    birthday: "050505",
-    levels
-  }
-];
-
 const sayHi = (_: Object, args: any): string => `Hi ${args.name}`;
-const users = () => usersArr;
 
 export const userQueries = {
-  sayHi,
-  users
+  sayHi
 };
